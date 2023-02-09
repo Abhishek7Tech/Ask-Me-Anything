cab610bf75eb86343d69537b876dd0e968b2f5ee
Abhishek7tech <abhishektab45@gmail.com>
2023-02-09 11:02:00 +0530

    added Loading... state to button

--- src/components/searchbox/searchBox.tsx
+++ src/components/searchbox/searchBox.tsx
@@ -2,30 +2,40 @@ import React, { useState } from "react";
 import "../../css/app.css";
 import getAnswer from "../../api/api";
 
-type ACTIONTYPE = {text:(string:string) => void};
+type ACTIONTYPE = { text: (string: string) => void };
 
-const SearchBox = (props:ACTIONTYPE) => {
+const SearchBox = (props: ACTIONTYPE) => {
   const [input, setInput] = useState<string>("");
   const [token, setToken] = useState<number>(0);
+  const [click, setClick] = useState<string>(" Ask! ");
 
-  const InputHandler = async (event:React.FormEvent) => {
+  const InputHandler = async (event: React.FormEvent) => {
     event.preventDefault();
     console.log("props");
     if (input.length > 1) {
+      setClick("Loading...");
       const ans = await getAnswer(input);
       props.text(ans.choices[0].text);
       const tokens = ans.usage.completion_tokens;
       setToken(+tokens + token);
+      setClick("Ask!")
     } else {
       alert("Is this even a question? 😂🤣😂");
     }
     (event.target as any).reset();
   };
 
-  const questionHandler = (event:React.FormEvent) => {
+  const questionHandler = (event: React.FormEvent) => {
     event.preventDefault();
     setInput((event.target as HTMLInputElement).value);
   };
+
+  // const clickHandler = (event: React.FormEvent) => {
+  //   event.preventDefault();
+  //   if (input) {
+  //     setClick("Loading...");
+  //   }
+  // };
 
   return (
     <form onSubmit={InputHandler} className="search">
@@ -37,7 +47,9 @@ const SearchBox = (props:ACTIONTYPE) => {
         ></input>
       </div>
       <div className="submit">
-        <button type="submit"> Ask! </button>
+        <button  type="submit">
+          {click}
+        </button>
       </div>
       <h3>Token Used: {token} </h3>
     </form>
